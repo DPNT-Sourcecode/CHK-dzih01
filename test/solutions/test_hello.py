f76dbdd9b76ca2ac158e2a98f8cb04b212edd2bc
sbx_user1059 <sbx_user1059@ip-10-12-68-43.eu-west-2.compute.internal>
2018-03-03 20:07:11 +0000

    Sat Mar 03 20:07:11 UTC 2018

--- test/solutions/test_hello.py
+++ test/solutions/test_hello.py
@@ -6,7 +6,7 @@ from lib.solutions.hello import hello
 class TestHello(unittest.TestCase):
     def test_hello(self):
         friend_name = "Pepe"
-        assert hello(friend_name) == "Hello, " + friend_name + "!"
+        assert hello(friend_name) == "Hello, %s!" % friend_name
 
 
 if __name__ == '__main__':
