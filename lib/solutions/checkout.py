55668139b426fdfbcccfa8185ccc957b7b32fbba
sbx_user1059 <sbx_user1059@ip-10-11-197-82.eu-west-2.compute.internal>
2018-03-04 16:52:52 +0000

    Sun Mar 04 16:52:52 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,22 @@
 
+prices = {'A': {1: 50, 3: 130}
+          'B': {1: 30, 2: 45}
+          'C': {1: 20}
+          'D': {1: 15}}
+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    ticket = {}
+
+    for sku in skus:
+        ticket.setdefault(sku, )
+
+
+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
