a739a1d46aee890c9b9660728253d74d731c6c75
sbx_user1059 <sbx_user1059@ip-10-11-197-82.eu-west-2.compute.internal>
2018-03-04 19:30:27 +0000

    Sun Mar 04 19:30:27 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -10,40 +10,6 @@ PRICES = {'A': {1: 50, 3: 130, 5: 200},
           'H': {1: 10, 5: 45, 10: 80},
           'I': {1: 35},
           'J': {1: 60},
-          'K': {1: 80, 2: 150},
-          'L': {1: 90},
-          'M': {1: 15},
-          'N': {1: 40, 3: {'offer': 'one_free',
-                           'over': 'M'}},
-          'O': {1: 10},
-          'P': {1: 50, 5: 200},
-          'Q': {1: 30, 3: 80},
-          'R': {1: 50, 3: {'offer': 'one_free',
-                           'over': 'Q'}},
-          'S': {1: 30},
-          'T': {1: 20},
-          'U': {1: 40, 3: {'offer': 'one_free',
-                           'over': 'U'}},
-          'V': {1: 50, 2: 90, 3: 130},
-          'W': {1: 20},
-          'X': {1: 90},
-          'Y': {1: 10},
-          'Z': {1: 50}
-          }
-
-
-PRICES = {'A': {1: 50, 3: 130, 5: 200},
-          'B': {1: 30, 2: 45},
-          'C': {1: 20},
-          'D': {1: 15},
-          'E': {1: 40, 2: {'offer': 'one_free',
-                           'over': 'B'}},
-          'F': {1: 10, 2: {'offer': 'one_free',
-                           'over': 'F'}},
-          'G': {1: 20},
-          'H': {1: 10, 5: 45, 10: 80},
-          'I': {1: 35},
-          'J': {1: 60},
           'K': {1: 70, 2: 120},
           'L': {1: 90},
           'M': {1: 15},
@@ -74,6 +40,8 @@ PRICES = {'A': {1: 50, 3: 130, 5: 200},
                            'over':  ['S', 'T', 'X', 'Y', 'Z'],
                            'group_price': 45}}}
 
+
+GROUP_OFFER = ['S', 'T', 'X', 'Y', 'Z']
 
 def apply_group_offers(ticket):
     pass
@@ -109,8 +77,6 @@ def checkout(skus):
                             ticket[offer_over] -= 1
                             if ticket[offer_over] < 0:
                                 ticket[offer_over] = 0
-
-                elif PRICES[sku][offer]['offer'] == 'buy_any_three':
 
             else:
                 times = ticket[sku] / offer
